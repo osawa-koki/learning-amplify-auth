1f88d9acfd243e7681c4ef9d304f03e75e61aa23
osawa-koki <koki@osawa.dev>
2023-11-17 00:10:36 +0900

    プロジェクトの初期化。

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  ignorePatterns: [
+    '.next',
+    'amplify',
+    'build',
+    'dist',
+    'next-env.d.ts',
+    'node_modules',
+    'out'
+  ],
+  rules: {
+  }
+}
