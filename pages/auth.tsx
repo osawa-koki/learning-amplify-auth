94c1fd22d9486bccef62675a94e125aa7c0fee8e
osawa-koki <koki@osawa.dev>
2023-11-17 20:14:41 +0900

    認証情報を表示するように。

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { type AuthUser } from 'aws-amplify/auth'
+import { Button, Table } from 'react-bootstrap'
 
 interface Props {
   user: AuthUser
@@ -11,6 +12,26 @@ export default function App (props: Props): React.JSX.Element {
 
   return (
     <>
+      <Table bordered>
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Username</td>
+            <td>{user.username}</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>{user.signInDetails?.loginId}</td>
+          </tr>
+        </tbody>
+      </Table>
+      <hr />
+      <Button variant='primary' onClick={signOut}>Sign Out</Button>
     </>
   )
 }
